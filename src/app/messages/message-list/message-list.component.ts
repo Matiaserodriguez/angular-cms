a2d9d3c62dbeed686275f3fa6fdef27ad69f4f20
matias <matias@Matias.local>
2022-11-17 22:45:04 -0300

    Adds node server, week 10

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/contacts/contact.service';
 import { Message } from '../message.model';
 import { MessageService } from '../message.service';
 
@@ -11,9 +12,13 @@ import { MessageService } from '../message.service';
 export class MessageListComponent implements OnInit {
   @Input() messages: Message[] = [];
 
-  constructor(private messageService: MessageService) { };
+  constructor(
+    private messageService: MessageService,
+    private contactService: ContactService  
+  ) { };
 
   ngOnInit(): void {
+    this.contactService.getContacts()
     this.messages = this.messageService.getMessages()
     this.messageService.messageListChangedEvent
       .subscribe(item => this.messages = item)
